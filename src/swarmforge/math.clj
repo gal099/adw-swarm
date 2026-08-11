2c10d7bb35afe061b4505dc2317699dc83743bda
Juan Baez <juanbaez@MAC065.local>
2026-08-11 16:14:46 -0300

    Harden math/add with mutation manifest and ignore tool artifacts

--- src/swarmforge/math.clj
+++ src/swarmforge/math.clj
@@ -4,3 +4,7 @@
   "Returns the sum of two numbers."
   [a b]
   (+ a b))
+
+;; clj-mutate-manifest-begin
+;; {:version 1, :tested-at "2026-08-11T16:10:46.400838-03:00", :module-hash "-1910939792", :forms [{:id "form/0/ns", :kind "ns", :line 1, :end-line 1, :hash "965999988"} {:id "defn/add", :kind "defn", :line 3, :end-line 6, :hash "-977996697"}]}
+;; clj-mutate-manifest-end
